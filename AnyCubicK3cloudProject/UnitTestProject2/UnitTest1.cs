a059a02f65e3c22e66a6fa17b4b9ac27c63ee826
Lance20220209 <lancehuang@anycubic.com>
2022-02-09 17:50:55 +0800

    第一次提交

---
+++ AnyCubicK3cloudProject/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnyCubicK3cloudProject;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ZWLF_CreateErpOrderTest zWLF_CreateErpOrderTest = new ZWLF_CreateErpOrderTest();
+            zWLF_CreateErpOrderTest.CreateErpOrderTest();
+
+        }
+    }
+}
